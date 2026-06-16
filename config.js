9eccbb38cb000207a7eeffc29cfdf23fe15b1df1
Kumail Rizvi <kumail149@hotmail.com>
2026-06-15 23:55:14 -0400

    added the missing , in config

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // 3) Keep this file in the app root when deploying
 window.SARAB_CONFIG = {
   SUPABASE_URL: 'https://wdqcbewhssxkrikzcyux.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkcWNiZXdoc3N4a3Jpa3pjeXV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNzM4NTgsImV4cCI6MjA5MzY0OTg1OH0.BD-sYAJXXJgpkKu22V535yS-gfdepeT7U2Yshno3-9Q'
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkcWNiZXdoc3N4a3Jpa3pjeXV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNzM4NTgsImV4cCI6MjA5MzY0OTg1OH0.BD-sYAJXXJgpkKu22V535yS-gfdepeT7U2Yshno3-9Q',
 
 POSTHOG: {
   ENABLED: true,
